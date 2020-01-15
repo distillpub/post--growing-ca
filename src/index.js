f95ffdc8e6ca86ed61c83c454e0db2377f99cda4
Alexander Mordvintsev <moralex@google.com>
2020-01-15 12:14:19 +0100

    disable example figure

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 import * as _unused from "raw-loader!./index.ejs";
 // TODO: disable before publishing
 
-import Example from "./diagrams/svelte-example.svelte";
+// import Example from "./diagrams/svelte-example.svelte";
 
-// lazily initialize any diagram below the fold. E.G:
-const exampleTag = document.getElementById("svelte-example-dfigure");
-let example;
-exampleTag.addEventListener("ready", () => {
-	const target = exampleTag.querySelector("#svelte-example-target");
-	example = new Example({ target });
-});
+// // lazily initialize any diagram below the fold. E.G:
+// const exampleTag = document.getElementById("svelte-example-dfigure");
+// let example;
+// exampleTag.addEventListener("ready", () => {
+// 	const target = exampleTag.querySelector("#svelte-example-target");
+// 	example = new Example({ target });
+// });
