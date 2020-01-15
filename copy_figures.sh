6b20f5fbaf00371605ea86e107b709bf2f44c06b
Alexander Mordvintsev <moralex@google.com>
2020-01-15 12:07:17 +0100

    Async section, remove gifs, scripts

---
+++ copy_figures.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp -Rvu ~/Drive/My\ Drive/selforg/ca_growth/figures/*.{mp4,svg,png,jpg} ./static/figures
---
+++ make_gifs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+palette="/tmp/palette.png"
+
+for f in static/figures/*.mp4; do
+  gif="${f%%.*}.gif"
+  if [ ! -f "$gif" ] || [ "$f" -nt "$gif" ]; then
+    echo $f
+    ffmpeg -v warning -i "$f" -vf "palettegen" -y $palette
+    ffmpeg -v warning -i "$f" -i $palette -lavfi "paletteuse" -r 2 -y "$gif"
+  fi
+done
