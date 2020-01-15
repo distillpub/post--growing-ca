f82f605f6a63ea5d7dfb5c6b253d5ab72bca3890
Alexander Mordvintsev <moralex@google.com>
2020-01-15 17:55:41 +0100

    demo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+// import * as tf from '@tensorflow/tfjs';
+
 // Hot reloading
-import * as _unused from "raw-loader!./index.ejs";
 // TODO: disable before publishing
+import * as _unused from "raw-loader!./index.ejs";
 
-// import Example from "./diagrams/svelte-example.svelte";
+// import Example from "./diagrams/demo.html";
 
-// // lazily initialize any diagram below the fold. E.G:
 // const exampleTag = document.getElementById("svelte-example-dfigure");
 // let example;
 // exampleTag.addEventListener("ready", () => {
-// 	const target = exampleTag.querySelector("#svelte-example-target");
-// 	example = new Example({ target });
+//  	const target = exampleTag.querySelector("#svelte-example-target");
+//  	example = new Example({ target });
 // });
