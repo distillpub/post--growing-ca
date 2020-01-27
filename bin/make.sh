c1780b70a0168f836ac7a456eec127a97cf07f0d
Alexander Mordvintsev <zzznah@gmail.com>
2020-01-27 22:47:32 +0100

    ca.js; don't run ca loop offscreen

--- bin/make.sh
+++ bin/make.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # copy the last export.html from downloads
-cp -v "$(ls -t  ~/Downloads/export*.html | head -1)" article.html
+#cp -v "$(ls -t  ~/Downloads/export*.html | head -1)" article.html
+src="$(ls -t  ~/Downloads/export*.html | head -1)"
+if [ "$src" -nt article.html ]; then
+  cp -vf "$src" article.html
+fi
 cat header.html article.html footer.html >public/index.html
 echo "Build finished"
