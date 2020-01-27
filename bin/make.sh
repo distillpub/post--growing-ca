6e6aa4d406ebb52e57995f2a2016ba6bbc6c0210
Alexander Mordvintsev <zzznah@gmail.com>
2020-01-27 15:10:38 +0100

    rotation

--- bin/make.sh
+++ bin/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # copy the last export.html from downloads
-cp -uv "$(ls -t  ~/Downloads/export*.html | head -1)" article.html
+cp -v "$(ls -t  ~/Downloads/export*.html | head -1)" article.html
 cat header.html article.html footer.html >public/index.html
 echo "Build finished"
