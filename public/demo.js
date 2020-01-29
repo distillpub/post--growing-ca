e35bd5c2a5998362d7bff955698562792bdd4bda
Alexander Mordvintsev <moralex@google.com>
2020-01-29 18:08:55 +0100

    demo ui

--- public/demo.js
+++ public/demo.js
@@ -31,49 +31,95 @@ export function createDemo(divId) {
       $$('#experimentSelector *').forEach(e=>{
         e.style.backgroundColor = e.id<=experiment?'Gold':'';
       });
-      $('#play').style.display = paused? "block" : "none";
-      $('#pause').style.display = !paused? "block" : "none";
+      $('#play').style.display = paused? "inline" : "none";
+      $('#pause').style.display = !paused? "inline" : "none";
+      const speed = parseInt($('#speed').value);
+      $('#speedLabel').innerHTML = 'speed: '+
+          ['1/60 x', '1/10 x', '1/2 x', '1x', '2x', '4x', '<b>full throttle !!!</b>'][speed+3];
+
     }
 
-    $('#playPause').onclick = ()=>{
-      paused = !paused;
+    function initUI() {
+      $('#playPause').onclick = ()=>{
+        paused = !paused;
+        updateUI();
+      };
+      $('#resetButton').onclick = demo.reset;
+      $('#speed').onchange = updateUI;
+      $('#speed').oninput = updateUI;
+      $('#experimentSelector').onclick = e=>{
+        experiment = e.target.id;
+        updateModel();
+      };
+      const modelSel = $('#emojiSelector');
+      for (let c of '😀💥👁🦎🐠🦋🐞🕸🥨🎄') {
+        modelSel.innerHTML += `<span id="${c}">${c}</span>`;
+      }
+      modelSel.innerHTML += `<span id="planarian">planarian</span>`;
+      modelSel.onclick = async e => {
+        target = e.target.innerText;
+        if (target == 'planarian') {
+          experiment = 'ex3';
+        }
+        updateModel();
+      };
+
+      let doubleClick = false;
+
+      canvas.onmousedown = e => {
+        e.preventDefault();
+        const [x, y] = getMousePos(e);
+        if (e.buttons == 1) {
+          if (doubleClick) {
+            demo.paint(x, y, 1, 'seed');
+            doubleClick = false;
+          } else {
+            doubleClick = true;
+            setTimeout(()=>{
+              doubleClick = false;
+            }, 300);
+            demo.paint(x, y, 8, 'clear');
+          }
+        }
+      }
+      canvas.onmousemove = e => {
+        e.preventDefault();
+        if (e.buttons == 1) {
+          const [x, y] = getMousePos(e);
+          demo.paint(x, y, 8, 'clear');
+        }
+      }
+    
+      canvas.addEventListener("touchmove", e=>{
+        const ox = e.target.offsetLeft;
+        const oy = e.target.offsetTop;
+        for (const t of e.touches) {
+          const mx = t.pageX - ox;
+          const my = t.pageY - oy;
+          const [w, h] = demo.gridSize;
+          const x = Math.floor(mx / canvas.clientWidth * w);
+          const y = Math.floor(my / canvas.clientHeight * h);
+          demo.paint(x, y, 8, 'clear');
+        }
+      }, false);
       updateUI();
     }
-
 
     async function updateModel() {
       const r = await fetch(`${modelDir}/${experiment}_${target}.json`);
       const model = await r.json();
       if (!demo) {
         demo = createCA(gl, model);
-        // $('#visMode').innerHTML = demo.visModes.map(s=>`<option value="${s}">${s}</option>`).join();
-        $('#resetButton').onclick = demo.reset;
+        initUI();        
         requestAnimationFrame(render);
       } else {
         demo.setWeights(model);
         demo.reset();
       }
       updateUI();
-
     }
     updateModel();
   
-    const modelSel = $('#emojiSelector');
-    for (let c of '😀💥👁🦎🐠🦋🐞🕸🥨🎄') {
-      modelSel.innerHTML += `<span id="${c}">${c}</span>`;
-    }
-    modelSel.innerHTML += `<span id="planarian">planarian</span>`;
-    modelSel.onclick = async e => {
-      target = e.target.innerText;
-      if (target == 'planarian') {
-        experiment = 'ex3';
-      }
-      updateModel();
-    }
-    $('#experimentSelector').onclick = async e=>{
-      experiment = e.target.id;
-      updateModel();
-    }
   
     function getMousePos(e) {
       const [w, h] = demo.gridSize;
@@ -82,82 +128,35 @@ export function createDemo(divId) {
       return [x, y];
     }
   
-    let doubleClick = false;
-    function onClick(x, y) {
-
-    }
-
-
-    canvas.onmousedown = e => {
-      e.preventDefault();
-      if (!demo)
-        return;
-      const [x, y] = getMousePos(e);
-      if (e.buttons == 1) {
-        if (doubleClick) {
-          demo.paint(x, y, 1, 'seed');
-          doubleClick = false;
-        } else {
-          doubleClick = true;
-          setTimeout(()=>{
-            doubleClick = false;
-          }, 300);
-          demo.paint(x, y, 8, 'clear');
-        }
-      }
-    }
-    canvas.onmousemove = e => {
-      e.preventDefault();
-      if (!demo)
-        return;
-      if (e.buttons == 1) {
-        const [x, y] = getMousePos(e);
-        demo.paint(x, y, 8, 'clear');
-      }
-    }
-  
-    canvas.addEventListener("touchmove", e=>{
-      if (!demo)
-        return;
-      const ox = e.target.offsetLeft;
-      const oy = e.target.offsetTop;
-      for (const t of e.touches) {
-        const mx = t.pageX - ox;
-        const my = t.pageY - oy;
-        const [w, h] = demo.gridSize;
-        const x = Math.floor(mx / canvas.clientWidth * w);
-        const y = Math.floor(my / canvas.clientHeight * h);
-        demo.paint(x, y, 8, 'clear');
-      }
-    }, false);
-  
-    let lastDrawTime;
+    let lastDrawTime = 0;
     let stepsPerFrame = 1;
+    let frameCount = 0;
   
     function render(time) {
-      if (!demo)
-        return;
       if  (!isInViewport(canvas)) {
         requestAnimationFrame(render);
         return;
       }
   
-      let stepN = 0;
       if (!paused) {
-        stepN = 1;
-        if ($('#throttle').checked && lastDrawTime) {
-          if (time - lastDrawTime < 18) {
-            stepsPerFrame += 1;
-          } else {
-            stepsPerFrame = Math.max(1, stepsPerFrame-1);
-          }
-          stepN = stepsPerFrame;
+        const speed = parseInt($("#speed").value);
+        if (speed <= 0) {  // slow down by skipping steps
+          const skip = [1, 2, 10, 60][-speed];
+          stepsPerFrame = (frameCount % skip) ? 0 : 1;
+          frameCount += 1;
+        } else if (speed > 0) { // speed up by making more steps per frame
+          const interval = time - lastDrawTime;
+          stepsPerFrame += interval<20.0 ? 1 : -1;
+          stepsPerFrame = Math.max(1, stepsPerFrame);
+          stepsPerFrame = Math.min(stepsPerFrame, [1, 2, 4, Infinity][speed])
         }
-        lastDrawTime = time;
+      } else {
+        stepsPerFrame = 0;
       }
-      
+      lastDrawTime = time;
+
       demo.setAngle($('#angle').value);
-      for (let i=0; i<stepN; ++i) {
+      for (let i=0; i<stepsPerFrame; ++i) {
         demo.step();
       }
       twgl.bindFramebufferInfo(gl);
