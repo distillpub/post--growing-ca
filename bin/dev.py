84f1313bfe42b2d0fc1531c8ebecf6a47fc7bee3
Alexander Mordvintsev <moralex@google.com>
2020-01-30 10:48:21 +0100

    build system

--- bin/dev.py
+++ bin/dev.py
@@ -2,17 +2,37 @@
 
 Runs ./bin/make.sh whenever index.html is fetched.
 '''
-import os
+from __future__ import print_function
+
+import os, glob
 import six
 if six.PY3:
     from http.server import SimpleHTTPRequestHandler, test
 else:
     from SimpleHTTPServer import SimpleHTTPRequestHandler, test
 
+
+def build():
+    os.system('''
+    src="$(ls -t  ~/Downloads/export*.html | head -1)"
+    if [ "$src" -nt ../article.html ]; then
+        cp -vf "$src" ../article.html
+    fi
+    ''')
+    with open('index.html', 'w') as fout:
+        for s in open('../index.html'):
+            if s.startswith('%% '):
+                fn = '../'+s.split()[1]
+                fout.write(open(fn).read())
+            else:
+                fout.write(s)
+    print('build finished')
+
+
 class Handler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path in ['/', '/index.html']:
-            os.system('cd .. && bin/make.sh')
+            build()
         if six.PY3:
             super().do_GET()
         else:
@@ -20,5 +40,7 @@ class Handler(SimpleHTTPRequestHandler):
 
 if __name__ == '__main__':
     os.chdir('public')
+    build()
     test(HandlerClass=Handler)
+    
 
--- bin/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# copy the last export.html from downloads
-#cp -v "$(ls -t  ~/Downloads/export*.html | head -1)" article.html
-src="$(ls -t  ~/Downloads/export*.html | head -1)"
-if [ "$src" -nt article.html ]; then
-  cp -vf "$src" article.html
-fi
-cat header.html article.html footer.html >public/index.html
-echo "Build finished"
