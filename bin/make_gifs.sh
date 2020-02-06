f7038636fc0554bbbdfea0c91afc94a137acbae6
Alexander Mordvintsev <moralex@google.com>
2020-02-06 18:06:31 +0100

    upd, planaria

--- bin/make_gifs.sh
+++ bin/make_gifs.sh
@@ -2,8 +2,8 @@
 
 palette="/tmp/palette.png"
 
-for f in ./static/figures/*.mp4; do
-  gif="${f%%.*}.gif"
+for f in ./public/figures/*.mp4; do
+  gif="${f%.*}.gif"
   if [ ! -f "$gif" ] || [ "$f" -nt "$gif" ]; then
     echo $f
     ffmpeg -v warning -i "$f" -vf "palettegen" -y $palette
